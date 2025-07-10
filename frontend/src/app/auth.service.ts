ddd1c9d6b4bb354f3e6386c340b64b4d68157413
Reymar Loaiza Labarden <133164923+ReyMar81@users.noreply.github.com>
2025-07-10 12:52:18 -0400

    nuevo url de instancia

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -268,7 +268,7 @@ export class AuthService {
   public router = inject(Router);
 
   // --- Endpoints centralizados ---
-  private API_BASE = 'http://50.16.8.139:8000';
+  private API_BASE = 'http://3.94.110.59:8000';
   private apiUrl = `${this.API_BASE}/auth/token/`;
   private perfilUrl = `${this.API_BASE}/api/personas/perfil/`;
   private inscripcionUrl = `${this.API_BASE}/api/personas/inscripcion/`;
